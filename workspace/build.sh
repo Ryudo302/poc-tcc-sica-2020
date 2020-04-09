fabb874caa4dbce34b0f567b579d9949ec3ee15b
Thiago Miranda <th.colbert@gmail.com>
2020-04-09 01:25:35 -0300

    Habilitando build do Gitlab CI

--- workspace/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mvn clean install -f ./sica-parent/pom.xml
-mvn clean install -f ./driver-sensores/pom.xml
-mvn clean install -f ./sica-commons/pom.xml
-mvn clean package -f ./sica-ativos/pom.xml
-mvn clean package -f ./sica-monitoramento/pom.xml
-mvn clean package -f ./sica-seguranca/pom.xml
-mvn clean package -f ./sica-portal-web/pom.xml
