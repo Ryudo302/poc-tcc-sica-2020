ed75b84ab9cbf0406df75705aea72c4723bafbcb
Thiago Miranda <th.colbert@gmail.com>
2020-04-26 19:13:46 -0300

    [Portal] Adicionando "loading" enquanto um módulo é carregado

--- workspace/sica-portal-web/src/main/js/components/Modulo.jsx
+++ workspace/sica-portal-web/src/main/js/components/Modulo.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import Iframe from 'react-iframe';
+import { Redirect } from 'react-router-dom';
 
 class Modulo extends React.Component {
 
     constructor(props) {
         super(props);
-        
+
         this.state = {
+            carregando: true,
             modulo: props.modulo || {}
         };
+
+        this.onLoadHandler = this.onLoadHandler.bind(this);
+        this.iframe = React.createRef();
+    }
+
+    componentDidMount() {
+        this.iframe.current && (this.iframe.current.onload = this.onLoadHandler);
+    }
+
+    componentDidUpdate() {
+        this.iframe.current && (this.iframe.current.onload = this.onLoadHandler);
+    }
+
+    onLoadHandler(event) {
+        this.setState({
+            carregando: false
+        });
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
@@ -22,15 +42,23 @@ class Modulo extends React.Component {
     }
 
     render() {
-        return (
-            <Iframe url={this.state.modulo.url}
-                width="100%"
-                height="100%"
-                id="modulo"
-                className="full-window"
-                display="initial"
-                position="relative" />
-        );
+        if (this.state.modulo.id) {
+            return (
+                <>
+                    {this.state.carregando && <Spinner animation="border" variant="primary" className="carregando" />}
+                    <iframe src={this.state.modulo.url}
+                        width="100%"
+                        height="100%"
+                        id="modulo"
+                        className="full-window"
+                        display="initial"
+                        position="relative"
+                        ref={this.iframe} />
+                </>
+            );
+        } else {
+            return <Redirect to="/login" />
+        }
     }
 }
 
